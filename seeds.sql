5fd8f92bf1ed80be7d3a0805a54ecf39438fe129
Joey Davidson <j.davidson18@gmail.com>
2021-07-07 12:23:22 -0600

    server initiated

---
+++ seeds.sql
@@ -0,0 +1,31 @@
+INSERT INTO lakes (lake, acres, yearStocked, cutNum, cutLength, cutRange, brookNum, brookLength, brookRange, distance, elevGain) VALUES ('Arapahoe-Lower', '11', '2008', '5', '11.2', '8.5-14.4', '0', '0', '0', '7.6', '1376');
+
+INSERT INTO lakes (lake, acres, yearStocked, cutNum, cutLength, cutRange, brookNum, brookLength, brookRange, distance, elevGain) VALUES ('Crater Lake Mid-North', '12', '2012', '0', '0', '0', '21', '10', '6.7-12.5', '7.6', '1376');
+
+INSERT INTO lakes (lake, acres, yearStocked, cutNum, cutLength, cutRange, brookNum, brookLength, brookRange, distance, elevGain) VALUES ('Crater Lake Mid-South', '6', '2012', '0', '0', '0', '38', '8.6', '3.7-13.2', '4', '1211');
+
+INSERT INTO lakes (lake, acres, yearStocked, cutNum, cutLength, cutRange, brookNum, brookLength, brookRange, distance, elevGain) VALUES ('Crater Lake - Upper', '7', '2012', '21', '13.1', '5.4-16.9', '0', '0', '0', '10.2', '1435');
+
+INSERT INTO lakes (lake, acres, yearStocked, cutNum, cutLength, cutRange, brookNum, brookLength, brookRange, distance, elevGain) VALUES ('Clayton Lake', '4', '2012', '5', '8.8', '7.0-10.3', '0', '0', '0','3.2', '1555' );
+
+INSERT INTO lakes (lake, acres, yearStocked, cutNum, cutLength, cutRange, brookNum, brookLength, brookRange, distance, elevGain) VALUES ('Forest Lake #1', '5', '2008', '0', '0', '0', '11', '11.4', '8.5-15.7', '6.7', '978');
+
+INSERT INTO lakes (lake, acres, yearStocked, cutNum, cutLength, cutRange, brookNum, brookLength, brookRange, distance, elevGain) VALUES ('Forest Lake #3', '3', '2008', '0', '0', '0', '19', '9.1', '6.3-10.7', '8', '1344');
+
+INSERT INTO lakes (lake, acres, yearStocked, cutNum, cutLength, cutRange, brookNum, brookLength, brookRange, distance, elevGain) VALUES ('Heart Lake', '15', '2012', '17', '12.6', '5.8-17.1', '0', '0', '0', '8', '1344');
+
+INSERT INTO lakes (lake, acres, yearStocked, cutNum, cutLength, cutRange, brookNum, brookLength, brookRange, distance, elevGain) VALUES ('Iceberg North', '9', '2013', '9', '9.9', '7.7-13.1', '0', '0', '0', '8', '1344');
+
+INSERT INTO lakes (lake, acres, yearStocked, cutNum, cutLength, cutRange, brookNum, brookLength, brookRange, distance, elevGain) VALUES ('Iceburg South', '6', '2013', '0', '0', '0', '0', '0', '0', '8', '1344');
+
+INSERT INTO lakes (lake, acres, yearStocked, cutNum, cutLength, cutRange, brookNum, brookLength, brookRange, distance, elevGain) VALUES ('James Peak Lake', '7', '2013', '27', '9.2', '4.5-16.1', '0', '0', '0', '8', '1344');
+
+INSERT INTO lakes (lake, acres, yearStocked, cutNum, cutLength, cutRange, brookNum, brookLength, brookRange, distance, elevGain) VALUES ('Jenny Lake', '6', '2008', '0', '0', '0', '8', '9.6', '7.8-10.6', '8', '1344');
+
+INSERT INTO lakes (lake, acres, yearStocked, cutNum, cutLength, cutRange, brookNum, brookLength, brookRange, distance, elevGain) VALUES ('Little Echo Lake', '11', '2013', '0', '0', '0', '0', '0', '0', '8', '1344');
+
+INSERT INTO lakes (lake, acres, yearStocked, cutNum, cutLength, cutRange, brookNum, brookLength, brookRange, distance, elevGain) VALUES ('Lost Lake (Hessie TH)', '5', '2013', '0', '0', '0', '49', '9.6', '5.7-12.2', '8', '1344');
+
+INSERT INTO lakes (lake, acres, yearStocked, cutNum, cutLength, cutRange, brookNum, brookLength, brookRange, distance, elevGain) VALUES ('Rogers Pass Lake', '5', '2012', '10', '9.1', '5.9-11.6', '0', '0', '0', '8', '1344');
+
+
