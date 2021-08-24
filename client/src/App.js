7a72d93f22dced7d6c3a3a164d65aba1212cf1ff
Joey Davidson <j.davidson18@gmail.com>
2021-08-24 14:22:16 -0600

    adjusted FE and BE routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
+import "./App.css";
+
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React from "react";
+
+import "./App.css";
+
 import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path={["/", "/lakes"]}>
+        <Route exact path={"/"}>
+          <Home />
+        </Route>
+        <Route exact path={"/lakes"}>
           <Home />
         </Route>
       </Switch>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
+
 import API from "../utils/API";
 
 function Home() {
@@ -13,12 +13,7 @@ function Home() {
       .catch((err) => console.log(err));
   }
 
-  return (
-    <>
-      <Link to={"/lakes"}></Link>
-      HELLO FROM HOME PAGE
-    </>
-  );
+  return <>HELLO FROM HOME PAGE</>;
 }
 
 export default Home;
