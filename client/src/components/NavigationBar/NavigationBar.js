d85eb11ca19f83a5aa930e1bc34ba5b12644942b
Joey Davidson <j.davidson18@gmail.com>
2021-08-26 13:02:48 -0600

    navigation bar complete

--- client/src/components/NavigationBar/NavigationBar.js
+++ client/src/components/NavigationBar/NavigationBar.js
@@ -7,38 +7,45 @@ import Contain from "../Contain/Contain";
 import Col from "../Col/Col";
 import ColLogo from "../Col/Col";
 import JPWlogo from "./JPWlogo.png";
-// import styles from "./NavigationBar.module.css";
+import styles from "./NavigationBar.module.css";
 import Row from "../Row/Row";
 import Image from "react-bootstrap/Image";
 
 // import styles from "./NavigationBar.module.css";
 
-// import { StyleSheet, Text, View } from 'react-native'
-
 export default function NavigationBar() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar className={styles["navBorder"]}>
       <Container>
-        <Contain>
-          <Row>
-            <Col size="md-4">
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                  <Nav.Link href="#home">Home</Nav.Link>
-                  <Nav.Link href="#link">Pictures</Nav.Link>
-                </Nav>
+        <Row>
+          <Col size="md-4">
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse>
+              <Navbar.Collapse>
+                <Nav.Link className={styles["navColor"]} href="#home">
+                  Home
+                </Nav.Link>
+                <Nav.Link className={styles["navColor"]} href="#home">
+                  Lakes
+                </Nav.Link>
               </Navbar.Collapse>
-            </Col>
-            <ColLogo size="md-4">
-              <Image src={JPWlogo} fluid rounded />
-            </ColLogo>
-            <Col size="md-4">CHUP</Col>
-          </Row>
-        </Contain>
+            </Navbar.Collapse>
+          </Col>
+          <Col size="md-4">
+            <Image src={JPWlogo} fluid rounded />
+          </Col>
+          <Col size="md-4">
+            <Navbar.Collapse className="justify-content-end">
+              <Nav.Link className={styles["navColor"]} href="#home">
+                Login
+              </Nav.Link>
+              <Nav.Link className={styles["navColor"]} href="#home">
+                My Profile
+              </Nav.Link>
+            </Navbar.Collapse>
+          </Col>
+        </Row>
       </Container>
     </Navbar>
   );
 }
-
-// const styles = StyleSheet.create({})
