b0e48f3836518f6f403f2af4cd91f837d12b7d0f
Joey Davidson <j.davidson18@gmail.com>
2021-09-05 04:51:25 -0600

    cleaned up DropDown Bar component

--- client/src/components/DropdownBar/DropdownBar.js
+++ client/src/components/DropdownBar/DropdownBar.js
@@ -1,66 +1,15 @@
 import React, { useContext } from "react";
 import Dropdown from "react-bootstrap/Dropdown";
-import DropdownButton from "react-bootstrap/DropdownButton";
-// import MenuItem from "react-bootstrap/MenuItem";
+
 import styles from "./DropdownBar.module.css";
 import LakeContext from "../../utils/LakeContext";
-import Form from "react-bootstrap/Form";
-import { Alert, MenuItem } from "bootstrap";
 
 export default function DropdownBar(props) {
   const { lake, fish, lakeNames, selectedLake } = useContext(LakeContext);
 
-  {
-    console.log(lake);
-  }
-
-  // let currFishState = props.props.props.setFishArray;
-
-  let lakeSelect = "";
-
-  function handleSelect(e) {
-    // let newLakeData = lakeNames.filter((lake) => lake === e);
-
-    let newLakeData = lakeNames.filter((lake) => lake === e);
-
-    lakeSelect = newLakeData.join();
-
-    // let lakeData = lakeNames.find((lake) => {
-    //   let newLakeData = lakeNames.filter((lake) => lake === e);
-
-    //   lakeSelect = newLakeData.join();
-
-    //   return newLakeData === e;
-    // });
-    console.log(lakeSelect);
-
-    newFunction();
-  }
-
-  function newFunction() {
-    var found = lakeNames.find(function (data, index) {
-      if (data.lake === lakeSelect) return true;
-    });
-    getLake();
-  }
-
-  function getLake() {
-    var newFound = lake.data.find(function (data, index) {
-      if ((lake.data[0].lake = lakeSelect)) return true;
-    });
-
-    let selectedFish = newFound.fish;
-    console.log(newFound);
-
-    // currLakeState(newFound);
-    // currFishState(selectedFish, newFound);
-
-    return console.log(newFound);
-  }
-
   return (
     <>
-      <Dropdown onSelect={handleSelect} className={styles["center1"]}>
+      <Dropdown className={styles["center1"]}>
         <Dropdown.Toggle
           style={{ width: "100%" }}
           variant="secondary"
@@ -71,7 +20,7 @@ export default function DropdownBar(props) {
         <Dropdown.Menu
           style={{ width: "100%" }}
           className={styles["ddBar"]}
-          onSelect={handleSelect}
+          // onSelect={handleSelect}
         >
           {lakeNames.map((lakeName) => {
             return (
