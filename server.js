1a224b975157b6924e392be81828d293f18dcba1
Joey Davidson <j.davidson18@gmail.com>
2021-10-06 12:25:29 -0600

    heroku deploy take 14

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ console.log("process.env.node_env below");
 console.log(process.env.NODE_ENV);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
-
+  const root = require("path").join(__dirname, "client", "build");
+  app.use(express.static(root));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile("index.html", { root });
   });
 } else {
   app.get("/", (req, res) => {
