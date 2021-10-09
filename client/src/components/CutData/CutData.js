d77baffe67e6c28c4ad65e4301b30a5e32bc429f
Joey Davidson <j.davidson18@gmail.com>
2021-10-09 06:37:07 -0600

    deploy

--- client/src/components/CutData/CutData.js
+++ client/src/components/CutData/CutData.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect } from "react";
-import Card from "react-bootstrap/card";
+import Card from "react-bootstrap/Card";
 import Table from "react-bootstrap/Table";
 import ListGroup from "react-bootstrap/ListGroup";
 import LakeContext from "../../utils/LakeContext";
