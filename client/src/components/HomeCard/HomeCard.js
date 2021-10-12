825c0c26ef6fbb13fab4daad857dcbb38528eb85
Joey Davidson <j.davidson18@gmail.com>
2021-10-12 13:34:24 -0600

    added app description to homepage

--- client/src/components/HomeCard/HomeCard.js
+++ client/src/components/HomeCard/HomeCard.js
@@ -6,8 +6,13 @@ export default function HomeCard() {
   return (
     <Card className={styles["homeCard"]} style={{ width: "18rem" }}>
       <Card.Body>
-        <Card.Title>JAMES PEAK WILDNERESS LAKE FINDER</Card.Title>
-        <Card.Text>Please select a lake from the dropdown below!</Card.Text>
+        <Card.Title>JAMES PEAK WILDNERESS LAKE FINDER </Card.Title>
+        <Card.Title> </Card.Title>
+        <Card.Text>WELCOME!</Card.Text>
+        <Card.Text>
+          Every so often, Colorado Parks and Wildlife conducts a fish survey on
+          Colorado lakes. Pick a lake to see the results of it's fish survey!
+        </Card.Text>
       </Card.Body>
     </Card>
   );
